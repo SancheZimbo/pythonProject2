9f43992c270f49dec05cef69cf31c22d1a046e97
SancheZimbo <94455597+SancheZimbo@users.noreply.github.com>
2022-10-09 21:27:57 +0200

    Add files via upload

---
+++ RTA_homework.py
@@ -0,0 +1,7 @@
+Is_his_name_Sanchez_Manyika = True
+Is_his_student_ID_117415 = True
+
+if Is_his_name_Sanchez_Manyika or Is_his_student_ID_117415:
+    print("Give him 5 points")
+else:
+    print("Probably isn't your student")
